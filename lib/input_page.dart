cdcbb6e4adea6373a827289168ddcfb14d8f6598
Adharsh Reghu <adharshreghu2002@gmail.com>
2023-03-04 01:28:19 +0530

    botton bar added male and female icon added to the containers in the first row in the layout

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const bottomcontainercolor = Color(0xFFEB1555);
+const activecardcolor = Color(0xFF1D1E33);
+const bottomcontainerheght = 80.0;
 
 class InputPage extends StatefulWidget {
-  const InputPage({Key? key}) : super(key: key);
-
   @override
   State<InputPage> createState() => _InputPageState();
 }
@@ -20,55 +23,109 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                        color: Color(0xFF1D1F33),
-                        borderRadius: BorderRadius.circular(10)),
+                  child: ReusableCard(
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          FontAwesomeIcons.mars,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          "MALE",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0XFF8D8E98),
+                          ),
+                        )
+                      ],
+                    ),
+                    colour: activecardcolor,
                   ),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                        color: Color(0xFF1D1F33),
-                        borderRadius: BorderRadius.circular(10)),
+                  child: ReusableCard(
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.venus,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          "FEMALE",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0XFF8D8E98),
+                          ),
+                        )
+                      ],
+                    ),
+                    colour: activecardcolor,
                   ),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: Container(
-                margin: EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                    color: Color(0xFF1D1F33),
-                    borderRadius: BorderRadius.circular(10))),
+            child: ReusableCard(
+              cardChild: Column(
+                children: const [],
+              ),
+              colour: activecardcolor,
+            ),
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                        color: Color(0xFF1D1F33),
-                        borderRadius: BorderRadius.circular(10)),
+                  child: ReusableCard(
+                    cardChild: Column(
+                      children: const [],
+                    ),
+                    colour: activecardcolor,
                   ),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                        color: Color(0xFF1D1F33),
-                        borderRadius: BorderRadius.circular(10)),
+                  child: ReusableCard(
+                    cardChild: Column(
+                      children: const [],
+                    ),
+                    colour: activecardcolor,
                   ),
                 ),
               ],
             ),
           ),
+          Container(
+            color: bottomcontainercolor,
+            height: bottomcontainerheght,
+            width: double.infinity,
+          )
         ],
       ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({required this.colour, required this.cardChild});
+  final Widget cardChild;
+  final Color colour;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: cardChild,
+      margin: EdgeInsets.all(15),
+      decoration:
+          BoxDecoration(color: colour, borderRadius: BorderRadius.circular(10)),
     );
   }
 }
